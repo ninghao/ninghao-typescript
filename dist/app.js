135c34b083ce38fc374eaf2a340ccaf3fdd657fc
wanghao8080 <117663444@qq.com>
2018-07-29 11:43:06 +0800

    Enums

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,17 @@
 'use strict';
+/**
+ * TypeScript
+ */
+var PostStatus;
+(function (PostStatus) {
+    PostStatus[PostStatus["Unpublished"] = 0] = "Unpublished";
+    PostStatus[PostStatus["Published"] = 1] = "Published";
+    PostStatus[PostStatus["Draft"] = 2] = "Draft";
+})(PostStatus || (PostStatus = {}));
 var post = {
     title: 'One Day',
+    // content: '🛵 🌅 🏖 🍹 ⛰ 🌄',
+    status: PostStatus.Unpublished,
 };
+console.log(post);
+console.log(PostStatus[0]);
--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,23 @@
  * TypeScript
  */
 
+enum PostStatus {
+  Unpublished,
+  Published,
+  Draft,
+}
+
 interface Post {
   title: string;
   content?: string;
+  status: PostStatus;
 }
 
 let post: Post = {
   title: 'One Day',
   // content: '🛵 🌅 🏖 🍹 ⛰ 🌄',
+  status: PostStatus.Unpublished,
 }
+
+console.log(post);
+console.log(PostStatus[0]);
